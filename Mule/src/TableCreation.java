0ce1bb11783944677d54e526443ba7a15d7ffc42
DivyaPallanti <73708106+DivyaPallanti@users.noreply.github.com>
2021-11-02 10:10:22 +0530

    Add files via upload

--- Mule/src/TableCreation.java
+++ Mule/src/TableCreation.java
@@ -1,1 +1,12 @@
+import java.sql.*;
+public class TableCreation {
+	public static void main(String[] args)throws Exception
+	{
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+	    Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","system","system");
+	    PreparedStatement pstmt=con.prepareStatement("create table Moulana(Name varchar2(50),Hero_Name varchar2(50),Heroin_Name varchar2(50),director_name varchar(30))");
+	    pstmt.executeUpdate();
+	    con.close();
+	}
 
+}
